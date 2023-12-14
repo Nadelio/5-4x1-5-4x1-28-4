32db3227329b01930ac2029a9bcd473d96a34aec
Nadelio <138712393+Nadelio@users.noreply.github.com>
2023-12-14 08:33:14 -0600

    0800?

---
+++ run.java
@@ -0,0 +1,11 @@
+import newGame;
+
+public class run
+{
+  public static void main(String [] args)
+  {
+    Scanner p = new Scanner(System.in);
+    String input = p.nextLine();
+    newGame(input);
+  }
+}
